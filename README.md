da54b129ce3f230b4b26d4aeb2f608d85237e904
Madeleine Ball <mpball@gmail.com>
2016-10-26 10:16:58 -0400

    Convert to Flask/Heroku app

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 Proof of concept project to develop a web-based data transfer tool from
 Nightscout databases to Open Humans and other platforms.
 
-This website is intended for hosting on Github Pages at
-http://nightscoutfoundation.github.io/dataxfer
+This website is intended for deployment on Heroku at:
+https://dataxfer-ns-oh.herokuapp.com/
 
 ### Running locally
 
-To develop this site, you can run it locally with Jekyll (`jekyll serve`).
+To develop this site, you can run this app locally with foreman...
 
-Github's tips for setting this up:
-https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
+#### Creating a project on Open Humans
+
+If you want to develop locally, you'll probably want to create your own
+OAuth2 project in Open Humans to get a new `client_id` and
+`client_secret`.
+
+(You can't reuse the IDs for the project/app running on Heroku, because
+Open Humans requires OAuth2 projects to register their redirect_uri.)
+
+Follow the instructions here: https://www.openhumans.org/direct-sharing/oauth2-setup/
+
+**You should probably set your project's redirect URI to: http://127.0.0.1:5000/**
+&ndash; this is the URI the app will default to when you run Flask locally.
+
+#### Configure .env for OAuth2.
+
+Copy `env.example` to `.env`.
+
+Replace `CLIENT_ID` and `CLIENT_SECRET` to match your app.
+
+#### Install and run with foreman
+
+`Procfile` and `.env` files are used by foreman. You can install foreman
+to run this app locally.
+
+Read more about installing foreman here: https://github.com/ddollar/foreman
+
+Once installed, you can run this command in the project's base directory: `foreman start`
+
+Your local site can be loaded by opening a web browser and visiting http://127.0.0.1:5000/
---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+
+from flask import Flask, render_template, request
+import requests
+
+CLIENT_ID = os.getenv('OH_CLIENT_ID', '')
+CLIENT_SECRET = os.getenv('OH_CLIENT_SECRET', '')
+OH_BASE_URL = 'https://www.openhumans.org/'
+DATAXFER_BASE_URL = os.getenv('DATAXFER_BASE_URL', 'http://127.0.0.1:5000/')
+
+# Default to DEBUG as True.
+DEBUG = False if os.getenv('DEBUG', '').lower() == 'False' else True
+
+app = Flask(__name__)
+
+# Set up Flask app to log to stderr.
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+if DEBUG:
+    app.logger.setLevel(logging.DEBUG)
+else:
+    app.logger.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+
+def exchange_code(code):
+    if CLIENT_SECRET and CLIENT_ID and code:
+        data = {
+            'grant_type': 'authorization_code',
+            'redirect_uri': '{}complete'.format(DATAXFER_BASE_URL),
+            'code': code,
+        }
+        req = requests.post(
+            '{}oauth2/token/'.format(OH_BASE_URL),
+            data=data,
+            auth=requests.auth.HTTPBasicAuth(
+                CLIENT_ID,
+                CLIENT_SECRET
+            ))
+        if 'access_token' in req.json():
+            return req.json()['access_token']
+        elif 'error' in req.json():
+            app.logger.warning('Error in token exchange: {}'.format(req.json()))
+        else:
+            app.logger.warning('Neither token nor error info in OH response!')
+    else:
+        app.logger.warning('CLIENT_SECRET or code are unavailable')
+    return ''
+
+
+@app.route("/")
+def index():
+    app.logger.debug("Loading index.html")
+    return render_template('index.html', client_id=CLIENT_ID)
+
+
+@app.route("/complete")
+def complete():
+    code = request.args.get('code', '')
+    token = exchange_code(code=code)
+    return render_template('complete.html', code=code, token=token)
+
+
+if __name__ == "__main__":
+    app.run()
