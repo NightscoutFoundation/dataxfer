b461c93a5efc88ad92aa9837e6a68c158816580f
Madeleine Ball <mpball@gmail.com>
2016-11-18 17:13:04 -0500

    Get data from NS, upload to OH

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import os
 from flask import Flask, render_template, request
 import requests
 
+from ns_to_oh import ns_to_oh
+
 CLIENT_ID = os.getenv('OH_CLIENT_ID', '')
 CLIENT_SECRET = os.getenv('OH_CLIENT_SECRET', '')
 OH_BASE_URL = 'https://www.openhumans.org/'
 DATAXFER_BASE_URL = os.getenv('DATAXFER_BASE_URL', 'http://127.0.0.1:5000/')
+OH_PROJ_PAGE = 'https://www.openhumans.org/activity/nightscout-data-transfer/'
 
 # Default to DEBUG as True.
 DEBUG = False if os.getenv('DEBUG', '').lower() == 'False' else True
@@ -52,15 +55,23 @@ def exchange_code(code):
 @app.route("/")
 def index():
     app.logger.debug("Loading index.html")
-    return render_template('index.html', client_id=CLIENT_ID)
+    return render_template(
+        'index.html', client_id=CLIENT_ID, oh_proj_page=OH_PROJ_PAGE)
 
 
-@app.route("/complete")
+@app.route("/complete", methods=['GET', 'POST'])
 def complete():
-    code = request.args.get('code', '')
-    token = exchange_code(code=code)
-    return render_template('complete.html', code=code, token=token)
-
-
-if __name__ == "__main__":
-    app.run()
+    if request.method == 'GET':
+        app.logger.debug("Loading complete.html")
+        code = request.args.get('code', '')
+        token = exchange_code(code=code)
+        return render_template('complete.html', code=code, token=token)
+    elif request.method == 'POST':
+        app.logger.debug("Loading done.html")
+        token = request.form['accessToken']
+        before_date = request.form['beforeDate']
+        after_date = request.form['afterDate']
+        ns_url = request.form['nightscoutURL']
+        ns_to_oh(token, before_date, after_date, ns_url, logger=app.logger)
+        return render_template('done.html', nightscout_url=ns_url,
+                               access_token=token)
