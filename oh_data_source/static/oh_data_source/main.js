bfdb80d3294e131ada18c06588320733c113e8c7
jameno <jasonmeno@msn.com>
2017-08-21 23:54:54 -0400

    jQuery date selection added

--- oh_data_source/static/oh_data_source/main.js
+++ oh_data_source/static/oh_data_source/main.js
@@ -1,1 +1,44 @@
 // Placeholder file for local JavaScript.
+
+$(document).ready(function(){
+	alert("Javascript is loaded!");
+	$(function() {
+		$('input[name="afterDate"]').daterangepicker({
+		    singleDatePicker: true,
+		    showDropdowns: true,
+			autoUpdateInput: false,
+			cancelLabel: 'Clear'
+		}, 
+		function(start, end, label) {
+		    
+		});
+	});
+
+	$('input[name="afterDate"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('YYYY-MM-DD'));
+  	});
+
+	$('input[name="afterDate"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+	});
+
+	$(function() {
+		$('input[name="beforeDate"]').daterangepicker({
+		    singleDatePicker: true,
+		    showDropdowns: true,
+			autoUpdateInput: false,
+			cancelLabel: 'Clear'
+		}, 
+		function(start, end, label) {
+		    
+		});
+	});
+
+	$('input[name="beforeDate"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('YYYY-MM-DD'));
+  	});
+
+	$('input[name="beforeDate"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+	});
+}); // end of $(document).ready()
